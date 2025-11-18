4b9fe21186bc9ac77c2a631789f2918e2dc2c071
Vivek <vivekkad2002@gmail.com>
2025-11-18 17:50:34 +0530

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -24,13 +24,14 @@ export const projects = [
   {
     title: "Restaurant",
     description:
-      "Weather Watch India is a responsive web app built with React that provides real-time weather updates for cities across India. It displays temperature, humidity, wind speed, and conditions using live API data in a clean, user-friendly interface.",
+      "Bite & Bliss is a modern, fully responsive restaurant website built with React.js and Tailwind CSS. It uses a modular component architecture, clean state management with React Hooks, and optimized layouts for mobile, tablet, and desktop. UI components are reusable, maintainable, and styled with Material UI icons for consistency. Menu data is pulled from a separate module for scalability and easy updates. The system is lightweight, fast, and structured for production-ready deployment.",
     tags: ["React.js", "Tailwind CSS"],
     liveUrl: "https://vocal-gecko-4ff66f.netlify.app/#home",
     githubUrl: "https://github.com/vivekkad1/React-projects/tree/main/Restaurant",
     imageUrl: Restaurant,
   },
 ];
+
 
 
 
